0ea217b97e8d7270abcfb54019a97891147fd577
dchandlerp <chandler.prince@gmail.com>
2017-07-20 13:58:16 -0500

    Added index.hmtl and category controller

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -3,7 +3,10 @@ package org.launchcode.controllers;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by davidprince on 7/20/17.
@@ -15,5 +18,10 @@ public class CategoryController {
     @Autowired // Creating a class that implements CategoryDao and putting one of those objects in the categoryDao field
     private CategoryDao categoryDao;
 
-
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, @RequestParam(defaultValue = "0") int id) {
+        model.addAttribute("title", "Categories");
+        model.addAttribute("categories", categoryDao.findAll());
+        return "category/index";
+    }
 }
